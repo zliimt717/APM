44f3e0404b37389b90eaeaa82b5121074e6d6d50
xin.gu <xin.gu@accenture.com>
2022-11-09 14:36:10 +0100

    change some code

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  currentUser:User;
 
   constructor() { }
 }
